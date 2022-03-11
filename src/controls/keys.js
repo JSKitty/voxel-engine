9f5ce40ec69940e436de5c442258d2eab242eeb1
JSKitty <jskitty@protonmail.com>
2022-03-11 16:44:08 +0000

    [Engine] Initial voxel engine
    The initial voxel engine, with easy texture/mats/block-type handling, a grid system with it's own easy wrappers, and a 'cell' generator to show the system at work!

---
+++ src/controls/keys.js
@@ -0,0 +1,47 @@
+
+/* Bindings */
+const controller = {
+    'w': {pressed: false, func: camForwards},
+    'a': {pressed: false, func: camLeft},
+    's': {pressed: false, func: camBackwards},
+    'd': {pressed: false, func: camRight}
+}
+
+/* Movement Controls */
+const camDir = new THREE.Vector3();
+function camForwards() {
+	camera.getWorldDirection(camDir);
+	camera.position.addScaledVector(camDir, 0.1);
+}
+
+function camBackwards() {
+	camera.getWorldDirection(camDir);
+	camera.position.addScaledVector(camDir, -0.1);
+}
+
+function camLeft() {
+    camera.translateX(-0.1);
+}
+
+function camRight() {
+    camera.translateX(0.1);
+}
+
+
+/* Key Event Handlers */
+const handleKeyDown = (e) => {
+    controller[e.key] && (controller[e.key].pressed = true)
+}
+
+const handleKeyUp = (e) => {
+    controller[e.key] && (controller[e.key].pressed = false)
+}
+
+const runPressedButtons = () => {
+    Object.keys(controller).forEach(key => {
+        controller[key].pressed && controller[key].func()
+    })
+}
+
+document.addEventListener("keydown", handleKeyDown);
+document.addEventListener("keyup", handleKeyUp);
---
+++ src/main.js
@@ -0,0 +1,116 @@
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(90, window.innerWidth/window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// White directional light used as 'sunlight'
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.75);
+directionalLight.castShadow = true;
+scene.add(directionalLight);
+
+// Ambient lighting used to prevent total black-out for unlit faces
+const light = new THREE.AmbientLight(0x404040); // soft white light
+scene.add(light);
+
+// Setup the camera
+camera.position.x = 10;
+camera.position.z = 15;
+camera.position.y = 10;
+camera.lookAt(scene.position);
+
+// Prepare our textures
+const loader = new THREE.TextureLoader();
+const textures = {
+    'dirt': loader.load('src/textures/dirt.jpg'),
+    'grassTop': loader.load('src/textures/grass-top.png'),
+    'grassSide': loader.load('src/textures/grass-side.jpg')
+}
+// Apply filters, etc!
+Object.entries(textures).forEach(tex => {
+    tex[1].magFilter = THREE.NearestFilter;
+    tex[1].minFilter = THREE.NearestFilter;
+});
+
+// Prepare our materials
+const materials = {
+    'dirt': new THREE.MeshStandardMaterial({ map: textures.dirt }),
+    'grass': [ // Side
+        new THREE.MeshStandardMaterial({ map: textures.grassSide }), // Side
+        new THREE.MeshStandardMaterial({ map: textures.grassSide }), // Side
+        new THREE.MeshStandardMaterial({ map: textures.grassTop }),  // Top
+        new THREE.MeshStandardMaterial({ map: textures.dirt }),      // Bottom
+        new THREE.MeshStandardMaterial({ map: textures.grassSide }), // Side
+        new THREE.MeshStandardMaterial({ map: textures.grassSide })  // Side
+    ]
+}
+
+// Prepare our geometry and meshes
+const blockGeoGlobal = new THREE.BoxGeometry(1, 1, 1);
+const blocks = {
+    'dirt': () => new THREE.Mesh(blockGeoGlobal, materials.dirt),
+    'grass': () => new THREE.Mesh(blockGeoGlobal, materials.grass)
+}
+
+// A three-dimensional world grid
+const grid = [[[]]];
+
+function addBlock(x, y, z, blockType) {
+    // Sanity: ensure this grid path exists
+    if (!grid[x]) grid[x] = [];
+    if (!grid[x][y]) grid[x][y] = [];
+    // Create a new block instance and assign it
+    const mesh = blockType();
+    grid[x][y][z] = mesh;
+    mesh.position.set(x, y, z);
+    // Spawn into the scene!
+    scene.add(mesh);
+}
+
+function removeBlock(x, y, z) {
+    // Sanity: ensure this grid block exists
+    if (grid[x] && grid[x][y] && grid[x][y][z]) {
+        // Remove from the scene
+        scene.remove(grid[x][y][z]);
+        // Remove from memory and free up it's coordinate
+        grid[x][y][z] = undefined;
+    }
+}
+
+function editBlock(x, y, z, matType) {
+    // Sanity: ensure this grid block exists
+    if (grid[x] && grid[x][y] && grid[x][y][z]) {
+        grid[x][y][z].material = matType;
+    }
+}
+
+// Generate a simple 'cell' of 8x8x8 blocks
+const cellSize = 8;
+for (let y = 0; y < cellSize; ++y) {
+    for (let z = 0; z < cellSize; ++z) {
+        for (let x = 0; x < cellSize; ++x) {
+            // Top-level blocks should be grass blocks!
+            addBlock(x, y, z, y === 7 ? blocks.grass : blocks.dirt);
+        }
+    }
+}
+
+// The renderer loop
+const render = function () {
+    runPressedButtons();
+    requestAnimationFrame(render);
+
+    renderer.render(scene, camera);
+};
+
+// Fire off the first renderer tick once ALL page elements are loaded
+window.onload = () => render();
+
+// If the page/screen size changes, make sure to adjust the rendering too!
+window.onresize = () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
