1993164abab04de3115d20dc00c37148a948c35f
JSKitty <jskitty@protonmail.com>
2022-03-13 18:36:07 +0000

    [Engine] Optimize material management
    Materials are now automatically cloned + disposed of at runtime! Meaning the engine no longer needs to "pre-allocate" materials as it did beforehand, this saves a TON of memory with large worlds, and reduces the memory footprint of the engine drastically.

--- src/main.js
+++ src/main.js
@@ -50,9 +50,9 @@ const materials = {
 // Prepare our geometry and meshes
 const blockGeoGlobal = new THREE.BoxGeometry(1, 1, 1);
 const blocks = {
-    'grass': () => new THREE.Mesh(blockGeoGlobal, materials.grass.map(a => a.clone())),
-    'dirt': () => new THREE.Mesh(blockGeoGlobal, materials.dirt.clone()),
-    'stone': () => new THREE.Mesh(blockGeoGlobal, materials.stone.clone()),
+    'grass': () => new THREE.Mesh(blockGeoGlobal, materials.grass),
+    'dirt': () => new THREE.Mesh(blockGeoGlobal, materials.dirt),
+    'stone': () => new THREE.Mesh(blockGeoGlobal, materials.stone),
 }
 
 // A three-dimensional world grid
@@ -75,6 +75,11 @@ function removeBlock(x, y, z) {
     if (grid[x] && grid[x][y] && grid[x][y][z]) {
         // Remove from the scene
         scene.remove(grid[x][y][z]);
+        // Check for cloned materials and dispose of them
+        if (grid[x][y][z].material.length)
+            grid[x][y][z].material.forEach(a => a.isCloned && a.dispose());
+        else if (grid[x][y][z].material.isCloned)
+            grid[x][y][z].material.dispose();
         // Remove from memory and free up it's coordinate
         grid[x][y][z] = undefined;
     }
@@ -83,17 +88,43 @@ function removeBlock(x, y, z) {
 function editBlock(x, y, z, matType) {
     // Sanity: ensure this grid block exists
     if (grid[x] && grid[x][y] && grid[x][y][z]) {
-        grid[x][y][z].material = matType;
+        if (matType.length)
+            grid[x][y][z].material = matType.forEach(a => a.clone());
+        else
+            grid[x][y][z].material = matType.clone();
     }
 }
 
 function setBlockColourIntensity(x, y, z, intensity) {
     // Sanity: ensure this grid block exists
     if (grid[x] && grid[x][y] && grid[x][y][z]) {
-        if (grid[x][y][z].material.length)
-            grid[x][y][z].material.forEach(a => a.color.setScalar(intensity));
-        else
-            grid[x][y][z].material.color.setScalar(intensity);
+        const cBlk = grid[x][y][z];
+        // Optimization Note:
+        // To save A LOT of memory: we'll only deep-clone materials on-demand,
+        // we also add a `.isCloned` flag to prevent double-clones in the future!
+        // Con: A slight 'hitch' in CPU during re-render of the new materials
+        if (cBlk.material.length) {
+            // Deep-Clone and reassign materials
+            if (!cBlk.material[0].isCloned) {
+                cBlk.material = cBlk.material.map(a => a.clone());
+                cBlk.material.forEach(a => {
+                    a.isCloned = true;
+                    // Apply internsity scalar to cloned materials
+                    a.color.setScalar(intensity);
+                })
+            } else {
+                // Apply internsity scalar to cloned materials
+                cBlk.material.forEach(a => a.color.setScalar(intensity));
+            }
+        } else {
+            // Deep-Clone and reassign material
+            if (!cBlk.material.isCloned) {
+                cBlk.material = cBlk.material.clone();
+                cBlk.material.isCloned = true;
+            }
+            // Apply internsity scalar to cloned material
+            cBlk.material.color.setScalar(intensity);
+        }
     }
 }
 
